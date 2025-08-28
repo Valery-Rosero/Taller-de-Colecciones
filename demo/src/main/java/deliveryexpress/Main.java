f87575f1ba2f5d2787c75ea70b511eb4580803d7
Valery-Rosero <128109786+Valery-Rosero@users.noreply.github.com>
2025-08-28 18:15:15 -0500

    Parte A - ArrayList y LinkedList2

--- demo/src/main/java/deliveryexpress/Main.java
+++ demo/src/main/java/deliveryexpress/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido a Delivery Express!");
         
-        // Ejecutar ejercicios de colecciones
         colecciones.Ejercicios.parteA_ArrayList();
         colecciones.Ejercicios.parteA_LinkedList();
     }
